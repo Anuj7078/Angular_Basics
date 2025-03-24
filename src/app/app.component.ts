d0a6609baa95e594a4494da9be33cfcd817f4180
Anuj Choudhary <anuj.choudhary.2711@gmail.com>
2025-03-24 10:54:25 +0530

    UC2 Hello World with Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']  // Reference to the SCSS file
+  styleUrls: ['./app.component.scss']  // If you're using SCSS
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
+  logoUrl = 'assets/BL_Logo.jpg';  // Path to the logo image
 }
